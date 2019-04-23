e189a019de7c6849190eb8a15fc297b4f1591eb9
Terry <terry.wade@valpo.edu>
2019-04-22 23:28:47 -0500

    Add Comments

--- mpipy.py
+++ mpipy.py
@@ -3,14 +3,21 @@ import threading
 import random
 import statistics
 
+#Initialize MPI environment
+#size = num processes (determined by -n command line flag)
+#rank = process the code is running on
+#name = name of the processor 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 name = MPI.Get_processor_name()
 
+#create a random list of integers 
 numList = list(random.sample(range(1,100), 20))
 
+#determines where to split the list 
 splitNum = len(numList)//size
+
 
 print("I am process number %d" % rank)
 if(rank == 0):
@@ -26,9 +33,11 @@ def taskmean(lst):
 def taskmedian(lst):
     print("Median: %d\n" % statistics.median(lst))
 
+#create tasks to run on threads
 t1 = threading.Thread(name='t1', target=taskmean, args=(workerList,))
 t2 = threading.Thread(name='t2', target=taskmedian, args=(workerList,))
 
+#run the tasks 
 t1.start()
 t2.start()
 
